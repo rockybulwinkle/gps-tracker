9d07e224ecd364d6e43ee100dee062d8525bc870
fendrirj <fendrirj@rose-hulman.edu>
2000-01-01 00:28:44 +0000

    bad sql version

--- gps_tracker.py
+++ gps_tracker.py
@@ -2,99 +2,171 @@
 #vim:tabstop=8:shiftwidth=4:smarttab:expandtab:softtabstop=4:autoindent:
 #vim:set nonumber
 import os
-
+import multiprocessing as mp
 GPS_PORT = '/dev/ttyO4'
-LOG_FILE = 'location.log'
+DB_FILE = 'gps_db'
 LOST_SIGNAL = 0
 from socketIO_client import SocketIO
 import serial
+import sqlite3
+import datetime
+import time
+import dateutil.parser
 
 def parse_gps(line):
 
-    line = line.split(',')
-    lat = line[2]
-    if lat:
-        print lat
-        lat = float(lat)
-        lat = str(lat/100)
-        print lat
-        lat_deg, lat_min = lat.split('.')
-        lat_deg = float(lat_deg)
-        lat_min = float("." + lat_min)
-        print lat_deg, lat_min
+	line = line.split(',')
+	lat = line[2]
+	if lat:
+		print lat
+		lat = float(lat)
+		lat = str(lat/100)
+		print lat
+		lat_deg, lat_min = lat.split('.')
+		lat_deg = float(lat_deg)
+		lat_min = float("." + lat_min)
+		print lat_deg, lat_min
 
-        lat = (lat_deg + lat_min/.60)
-        lat = -lat if "S" in line[3] else lat
-    else:
-        lat = LOST_SIGNAL
+		lat = (lat_deg + lat_min/.60)
+		lat = -lat if "S" in line[3] else lat
+	else:
+		lat = LOST_SIGNAL
 
-    long_ = line[4]
-    if long_:
-        print long_
-        long_ = float(long_)
-        long_ = str(long_/100)
-        print long_
-        long_deg, long_min = long_.split('.')
-        long_deg = float(long_deg)
-        long_min = float("." + long_min)
-        print long_deg, long_min
+	long_ = line[4]
+	if long_:
+		print long_
+		long_ = float(long_)
+		long_ = str(long_/100)
+		print long_
+		long_deg, long_min = long_.split('.')
+		long_deg = float(long_deg)
+		long_min = float("." + long_min)
+		print long_deg, long_min
 
-        long_ = (long_deg + long_min/.60)
-        long_ = -long_ if "W" in line[5] else long_
-    else:
-        long_ = LOST_SIGNAL
+		long_ = (long_deg + long_min/.60)
+		long_ = -long_ if "W" in line[5] else long_
+	else:
+		long_ = LOST_SIGNAL
+	
+	ret_val = {}
+	ret_val["fix_quality"] = line[6]
+	ret_val["num_sats"] = line[7]
+	ret_val["hdp"] = line[8]
+	ret_val["altitude"] = line[9]
+	ret_val["lat"] = lat
+	ret_val["long"] = long_
 
-    return (lat,long_)
+	return ret_val
+
+def parse_date(line):
+	line_ = line.split(',')
+	tod = line_[1].split('.')[0]
+	second = tod[-2:]
+	minute = tod[-4:-2]
+	hour = tod[-6:-4]
+
+	date_ = line_[9]
+	year = date_[-2:]
+	month = date_[-4:-2]
+	day = date_[-6:-4]
+	print hour,minute,second,year,day,month
+	t = datetime.datetime(*map(int,[year,month,day,hour,minute,second]))
+	return t
 
 def on_recv(*args):
-    print 'on_aaa_response', args
-
-def on_move(*args):
-    quit()
-    print "move!"
- 
-read_pipe, write_pipe = os.pipe()
-
-if os.fork() != 0: #is parent
-    os.close(read_pipe)
-    serial_port = serial.Serial(port=GPS_PORT, baudrate=9600)
-    gps_log = open(LOG_FILE, "r")
-    #for line in gps_log:
-        #os.write(write_pipe, line)
-    gps_log.close()
-    gps_log = open(LOG_FILE, "a")
-    #parse_gps("$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47")
-    for line in serial_port:
-        if "GPGGA" in line:
-            line = parse_gps(line)
-            line = map(str, line)
-            line = ','.join(line)
-            gps_log.write(line+"\n")
-            gps_log.flush()
-            os.write(write_pipe, line+"\n")
-            #os.close(write_pipe)
-    print "done parent"
+	print 'on_aaa_response', args
 
 
-else:
-    os.close(write_pipe)
-    socketIO = SocketIO('localhost', 8080)
-    socketIO.on('loc_recv', on_recv)
-    socketIO.on('move', on_move)
-    buffer = ""
-    while (True):
-        buffer += os.read(read_pipe, 1000)
-        line = buffer.split('\n')[0]
-        buffer = '\n'.join(buffer.split('\n')[1:])
 
-        line = line.split(',')
-        print line
-        lat,long_ = map(float,line)
-        print lat,long_
-        socketIO.emit('loc', {"lat":lat, "long":long_})
-        socketIO._transport.send_heartbeat()
-    print "done child"
+def serial_handler():
+	socketIO = SocketIO('localhost', 8080)
+	serial_port = serial.Serial(port=GPS_PORT, baudrate=9600)
+	last_date = None
+	conn = sqlite3.connect(DB_FILE)
+	cur = conn.cursor()
+	for line in serial_port:
+		socketIO._transport.send_heartbeat()
+		if "GPGGA" in line and last_date:
+			line = parse_gps(line)
+			cur.execute('insert into gps_data \
+				(date,long,lat,fix_quality,num_sats,\
+				hdp,altitude) values (?,?,?,?,?,?,?);'\
+				,[last_date,line["long"],line["lat"]\
+				,line["fix_quality"], line["num_sats"]\
+				,line["hdp"], line["altitude"]] )
+			conn.commit()
 
+			cur.execute("select key_value from\
+				config where key_name=\
+				'tracking.mode.type'")
+			tracking_mode_type = cur.fetchone()[0]
+			if tracking_mode_type=='true':
+				print "sending"
+				socketIO.emit('loc', line)
+
+		
+			
+			#os.close(write_pipe)
+		if "GPRMC" in line:
+			last_date = str(parse_date(line))
+
+def do_time_search(socket, cursor):
+	cursor.execute("select key_value from config where key_name='query.time.start'")
+	start_time = dateutil.parser.parse(cursor.fetchone()[0])
+	cursor.execute("select key_value from config where key_name='query.time.end'")
+	end_time = dateutil.parser.parse(cursor.fetchone()[0])
+	print start_time,end_time
+
+def search_handler():
+	conn = sqlite3.connect(DB_FILE)
+	cur = conn.cursor()
+	socketIO = SocketIO('localhost', 8080)
+	#socketIO.on('loc_recv', on_recv)
+	while (True):
+		cur.execute("select key_value from config where key_name='query.waiting'")
+		waiting = cur.fetchone()[0]
+		if waiting == "true":
+			cur.execute("update config set key_value='false'\
+			where key_name='query.waiting'")
+			cur.execute("select key_value from config where\
+			key_name='query.type'")
+			type = cur.fetchone()[0]
+			if type == 'time':
+				do_time_search(socketIO,cur)
+		time.sleep(1)
+		socketIO._transport.send_heartbeat()
+
+def settings_handler():
+	conn = sqlite3.connect(DB_FILE)
+	cur = conn.cursor()
+
+	def update_time_query(*args):
+		cur.execute("update config set\
+			key_value='time' where key_name=\
+			'query.type';")
+		cur.execute("update config set\
+			key_value=? where key_name=\
+			'query.time.start'", (args[0]["start"],))
+		cur.execute("update config set\
+			key_value=? where key_name=\
+			'query.time.end'", (args[0]["end"],))
+		cur.execute("update config set\
+			key_value='true' where key_name=\
+			'query.waiting'")
+		cur.close()
+		conn.commit()
+		conn.close()
+	
+	socketIO = SocketIO('localhost', 8080)
+	socketIO.on('time_query', update_time_query)
+	socketIO.wait()
+
+search_handler = mp.Process(target=search_handler)
+search_handler.start()
+serial_handler = mp.Process(target=serial_handler)
+serial_handler.start()
+settings_handler = mp.Process(target=settings_handler)
+settings_handler.start()
 #socketIO.emit('loc', {"lat":"86", "long_":"42"})
 #socketIO.wait(seconds=1)
 #socketIO.close()
