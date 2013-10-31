8dbc88222f7d00f7cb6bab6dce0c13740b769cac
fendrirj <fendrirj@rose-hulman.edu>
2013-10-31 19:05:53 +0000

    Added a webserver based on Mark Yoder's boneserver

---
+++ Map/audio.js
@@ -0,0 +1,157 @@
+    var socket;
+    var firstconnect = true,
+        fs = 8000,
+        Ts = 1/fs*1000,
+        samples = 800,
+        plotTop, plotBot,
+        globalDataL = [],
+        globalDataR = [];
+    globalDataL[samples] = 0;
+    globalDataR[samples] = 0;
+
+    function connect() {
+      if(firstconnect) {
+        socket = io.connect(null);
+
+        socket.on('audio', function(data){ audio(data); });
+        socket.on('connect', function()
+            { status_update("Connected to Server"); });
+        socket.on('disconnect', function()
+            { status_update("Disconnected from Server"); });
+        socket.on('reconnect', function()
+            { status_update("Reconnected to Server"); });
+        socket.on('reconnecting', function( nextRetry )
+            { status_update("Reconnecting in " + nextRetry/1000 + " s"); });
+        socket.on('reconnect_failed', function()
+            { message("Reconnect Failed"); });
+
+        firstconnect = false;
+      }
+      else {
+        socket.socket.reconnect();
+      }
+    }
+
+    function disconnect() {
+      socket.disconnect();
+    }
+
+    // When new data arrived, convert it and plot it.
+    function audio(data) {
+        var myData = window.atob(data);
+        var fftSize = 512;
+        var fftOutL, fftOutR;
+        // Pull out left and right channels
+        for(var i=0; i<samples; i++) {
+            globalDataL[i] = [i*Ts, myData.charCodeAt(2*i  )-128];
+            globalDataR[i] = [i*Ts, myData.charCodeAt(2*i+1)-128];
+        }
+        plotTop.setData([ globalDataL, globalDataR ]);
+        
+        fftOutL = fft(globalDataL, fftSize);
+        fftOutR = fft(globalDataR, fftSize);
+        plotBot.setData([ fftOutL, fftOutR ]);
+        // since the axes don't change, we don't need to call plot.setupGrid()
+        plotTop.draw();
+        plotBot.draw();
+
+      document.getElementById('message').innerHTML = "Server says: " +
+          globalDataL.length + " points: ";
+    }
+    
+    function fft(data, size) {
+        // Take the fft
+        var fftData = new Float32Array(size);
+        var fft = new FFT(size, fs);
+        var fftOut = new Array(size);
+        var i;
+        for (i = 0; i < size; i++) {
+            fftData[i] = data[i][1]; // Pull the y-values out
+        }
+        fft.forward(fftData);
+        for (i = 0; i < size; i++) {
+            fftOut[i] = [i*fs/size, 10*Math.log(fft.spectrum[i])/Math.LN10];
+        }
+        return fftOut;
+    }
+
+    function status_update(txt){
+      document.getElementById('status').innerHTML = txt;
+    }
+
+    function send(){
+      socket.send("Hello Server!");    
+    }
+
+// jQuery
+$(function () {
+
+    function initPlotData() {
+        // zip the generated y values with the x values
+        var result = [];
+        for (var i = 0; i <= samples; i++)
+            result[i] = [i*Ts, 0];
+        return result;
+    }
+
+    // setup control widget
+    var updateInterval = 100;
+    $("#updateInterval").val(updateInterval).change(function () {
+        var v = $(this).val();
+        if (v && !isNaN(+v)) {
+            updateInterval = +v;
+            if (updateInterval < 1)
+                updateInterval = 1;
+            if (updateInterval > 2000)
+                updateInterval = 2000;
+            $(this).val("" + updateInterval);
+        }
+    });
+
+    // setup plot
+    var optionsTop = {
+        series: { 
+            shadowSize: 0, // drawing is faster without shadows
+            points: { show: false},
+            lines:  { show: true, lineWidth: 3}
+        }, 
+        yaxis:  { min: -50, max: 50 },
+        xaxis:	{ show: true },
+        legend:	{ position: "ne" }
+    };
+    plotTop = $.plot($("#plotTop"), 
+        [ 
+          { data:  initPlotData(), 
+            label: "Left Channel" },
+          { data:  initPlotData(),
+            label: "Right Channel" }
+        ],
+            optionsTop );
+
+    var optionsBot = {
+        series: { 
+            shadowSize: 0, // drawing is faster without shadows
+            points: { show: false},
+            lines:  { show: true, lineWidth: 3}
+        }, 
+        yaxis:    { min: -25, max: 25}, 
+        xaxis:    { min: 0, max: 2000, show: true}, 
+        legend:	{ position: "ne" }
+    };
+    plotBot = $.plot($("#plotBot"), 
+        [ 
+          { data:  initPlotData(), 
+            label: "Left Channel" },
+          { data:  initPlotData(),
+            label: "Right Channel" }
+        ],
+            optionsBot);
+
+    // Request data every updateInterval ms
+    function update() {
+        socket.emit('audio', "Send more data");
+        setTimeout(update, updateInterval);
+    }
+    update();
+});
+
---
+++ Map/matrixLED.js
@@ -0,0 +1,112 @@
+    var socket;
+    var firstconnect = true,
+        i2cNum  = "0x70",
+	disp = [];
+
+// Create a matrix of LEDs inside the <table> tags.
+var matrixData;
+for(var j=7; j>=0; j--) {
+	matrixData += '<tr>';
+	for(var i=0; i<8; i++) {
+	    matrixData += '<td><div class="LED" id="id'+i+'_'+j+
+		'" onclick="LEDclick('+i+','+j+')">'+
+		i+','+j+'</div></td>';
+	    }
+	matrixData += '</tr>';
+}
+$('#matrixLED').append(matrixData);
+
+// Send one column when LED is clicked.
+function LEDclick(i, j) {
+//	alert(i+","+j+" clicked");
+    disp[i] ^= 0x1<<j;
+    socket.emit('i2cset', {i2cNum: i2cNum, i: i, 
+			     disp: '0x'+disp[i].toString(16)});
+//	socket.emit('i2c', i2cNum);
+    // Toggle bit on display
+    if(disp[i]>>j&0x1 === 1) {
+        $('#id'+i+'_'+j).addClass('on');
+    } else {
+        $('#id'+i+'_'+j).removeClass('on');
+    }
+}
+
+    function connect() {
+      if(firstconnect) {
+        socket = io.connect(null);
+
+        // See https://github.com/LearnBoost/socket.io/wiki/Exposed-events
+        // for Exposed events
+        socket.on('message', function(data)
+            { status_update("Received: message " + data);});
+        socket.on('connect', function()
+            { status_update("Connected to Server"); });
+        socket.on('disconnect', function()
+            { status_update("Disconnected from Server"); });
+        socket.on('reconnect', function()
+            { status_update("Reconnected to Server"); });
+        socket.on('reconnecting', function( nextRetry )
+            { status_update("Reconnecting in " + nextRetry/1000 + " s"); });
+        socket.on('reconnect_failed', function()
+            { message("Reconnect Failed"); });
+
+        socket.on('matrix',  matrix);
+        // Read display for initial image.  Store in disp[]
+        socket.emit("matrix", i2cNum);
+
+        firstconnect = false;
+      }
+      else {
+        socket.socket.reconnect();
+      }
+    }
+
+    function disconnect() {
+      socket.disconnect();
+    }
+
+    // When new data arrives, convert it and display it.
+    // data is a string of 16 values, each a pair of hex digits.
+    function matrix(data) {
+        var i, j;
+        disp = [];
+        //        status_update("i2c: " + data);
+        // Make data an array, each entry is a pair of digits
+        data = data.split(" ");
+        //        status_update("data: " + data);
+        // Every other pair of digits are Green. The others are red.
+        // Ignore the red.
+        // Convert from hex.
+        for (i = 0; i < data.length; i += 2) {
+            disp[i / 2] = parseInt(data[i], 16);
+        }
+        //        status_update("disp: " + disp);
+        // i cycles through each column
+        for (i = 0; i < disp.length; i++) {
+            // j cycles through each bit
+            for (j = 0; j < 8; j++) {
+                if (((disp[i] >> j) & 0x1) === 1) {
+                    $('#id' + i + '_' + j).addClass('on');
+                } else {
+                    $('#id' + i + '_' + j).removeClass('on');
+                }
+            }
+        }
+    }
+
+    function status_update(txt){
+	$('#status').html(txt);
+    }
+
+    function updateFromLED(){
+      socket.emit("matrix", i2cNum);    
+    }
+
+connect();
+
+$(function () {
+    // setup control widget
+    $("#i2cNum").val(i2cNum).change(function () {
+        i2cNum = $(this).val();
+    });
+});
