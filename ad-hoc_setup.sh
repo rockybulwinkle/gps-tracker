6c9f83a055fc0122932f8bffee718163aced69d7
Chris Hopwood <rockybulwinkle@gmail.com>
2013-11-02 23:36:00 +0000

    added script to setup ad-hoc network

---
+++ ad-hoc_setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#Run this file to setup an Ad-hoc wirless network. All machines that you want
+#to be involved in this network must run this file.
+
+
+if [ $# -ne 4 ] ; then
+	echo "Usage: $0 interface IP_address SSID frequency"
+	echo "This script must be run on each machine involved in the network"
+	echo "Each machine needs to have a unique IP Address but must be apart"
+	echo "of the same subnet (Subnet is fixed to /24)"
+	echo "Number of arguments given: $#"
+	exit 1
+fi
+
+interface=$1
+ipaddr=$2
+ssid=$3
+freq=$4
+
+#bring the interface down
+ip link set $interface down 
+#set the interface to ad-hoc mode
+iw $interface set type ibss
+#bring the interface back up
+ip link set $interface up 
+#set ssid
+iw $interface ibss join $ssid $freq 
+#set static ip
+ip addr add $ipaddr/24 dev $interface
