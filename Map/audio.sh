59cd54f16f6f98538429709b3afb5ac1c0093567
fendrirj <fendrirj@rose-hulman.edu>
2013-11-07 19:39:04 +0000

    Removed some of the extraneous files

--- Map/audio.sh
+++
@@ -1,7 +0,0 @@
-cd /home/root/exercises/node.js/realtime
-node ./boneServer.js &
-NODE_PID=$!
-sleep 1
-
-midori --app="localhost:8080/audio.html"
-kill -15 $NODE_PID
--- Map/setup.systemd.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Move the service file to the right place and enable it
-cp boneServer.service /lib/systemd/system
-systemctl start boneServer
-systemctl enable boneServer
